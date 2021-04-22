74c84ac1ce39523e9b24feb77f9208901d350ec2
Maalik Kaufhold <maalik.kaufhold@smail.th-koeln.de>
2021-04-22 13:35:27 +0200

    Finished Task 1.1 with answers

--- aufgabe_1_1/app/src/main/java/de/thkoeln/vma/prak1/gruppe_10/aufgabe_1_1/Activity_1.java
+++ aufgabe_1_1/app/src/main/java/de/thkoeln/vma/prak1/gruppe_10/aufgabe_1_1/Activity_1.java
@@ -3,7 +3,9 @@ package de.thkoeln.vma.prak1.gruppe_10.aufgabe_1_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,6 +15,7 @@ public class Activity_1 extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.v("STATUS", "onCreate() Activity_1");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
 
@@ -21,11 +24,45 @@ public class Activity_1 extends Activity {
         gotoSecondButton.setOnClickListener(new GotoSecondButtonListener());
 
     }
+
+    @Override
+    protected void onStart() {
+        Log.v("STATUS", "onStart() Activity_1");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.v("STATUS", "onResume() Activity_1");
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onPause() {
+        Log.v("STATUS", "onPause() Activity_1");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.v("STATUS", "onStop() Activity_1");
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.v("STATUS", "onRestart() Activity_1");
+        super.onRestart();
+    }
 }
 
 class GotoSecondButtonListener implements OnClickListener {
     @Override
     public void onClick(View v) {
-        Log.v("Message","Button was pressed");
+        Log.v("STATUS", "onClick() Button_2");
+        Intent myIntent = new Intent(v.getContext(), Activity_2.class);
+        v.getContext().startActivity(myIntent);
+
     }
 }
